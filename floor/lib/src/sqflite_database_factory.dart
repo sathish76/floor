6093e13cdf93189dce4c9ca3b1d4cd725e3257cb
Sathish <vsk7600@gmail.com>
2023-10-03 19:56:51 +0530

    build fix

--- floor/lib/src/sqflite_database_factory.dart
+++ floor/lib/src/sqflite_database_factory.dart
@@ -1,11 +1,16 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
 
 // infers factory as nullable without explicit type definition
 final DatabaseFactory sqfliteDatabaseFactory = () {
+  if (kIsWeb){
+    return databaseFactoryFfiWeb;
+  } 
   if (Platform.isAndroid || Platform.isIOS) {
     return databaseFactory;
   } else if (Platform.isLinux || Platform.isMacOS || Platform.isWindows) {
