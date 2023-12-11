8089c2d0c0b1949d4308b855b65a6db404a537a0
sathish76 <>
2023-12-11 22:00:12 +0530

    update pubspec

--- floor/lib/src/sqflite_database_factory.dart
+++ floor/lib/src/sqflite_database_factory.dart
@@ -3,19 +3,19 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
-import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart' as sqflite_ffi;
 import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
 
 // infers factory as nullable without explicit type definition
 final DatabaseFactory sqfliteDatabaseFactory = () {
-  if (kIsWeb){
+  if (kIsWeb) {
     return databaseFactoryFfiWeb;
-  } 
+  }
   if (Platform.isAndroid || Platform.isIOS) {
     return databaseFactory;
   } else if (Platform.isLinux || Platform.isMacOS || Platform.isWindows) {
-    sqfliteFfiInit();
-    return databaseFactoryFfi;
+    sqflite_ffi.sqfliteFfiInit();
+    return sqflite_ffi.databaseFactoryFfi;
   } else {
     throw UnsupportedError(
       'Platform ${Platform.operatingSystem} is not supported by Floor.',
